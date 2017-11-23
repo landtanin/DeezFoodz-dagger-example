17e5a8a7eddf6290cb1a2aa06ff7dfdf43534fdc
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-23 15:24:45 +0000

    added @Module annotatio to AppModule

--- app/src/main/java/com/raywenderlich/android/deezfoodz/dagger/AppModule.java
+++ app/src/main/java/com/raywenderlich/android/deezfoodz/dagger/AppModule.java
@@ -1,8 +1,16 @@
 package com.raywenderlich.android.deezfoodz.dagger;
 
+import dagger.Module;
+
 /**
  * Created by Tanin on 23/11/2017.
  */
 
+/**
+ * The @Module annotation tells Dagger that the AppModule class will provide dependencies for a part of the application.
+ */
+@Module
 public class AppModule {
+
+
 }
