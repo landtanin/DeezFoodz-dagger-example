3dde5dfcf263ab51a290fe78067fd61a9332d27e
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-23 16:41:39 +0000

    add provideRetrofit and provideUsdaApi methods to complete providing UsdaApi object

--- app/src/main/java/com/raywenderlich/android/deezfoodz/dagger/NetworkModule.java
+++ app/src/main/java/com/raywenderlich/android/deezfoodz/dagger/NetworkModule.java
@@ -1,6 +1,7 @@
 package com.raywenderlich.android.deezfoodz.dagger;
 
 import com.raywenderlich.android.deezfoodz.app.Constants;
+import com.raywenderlich.android.deezfoodz.network.UsdaApi;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -8,6 +9,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Converter;
+import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -29,6 +31,21 @@ public class NetworkModule {
     @Singleton
     Converter.Factory provideGsonConverter() {
         return GsonConverterFactory.create();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Converter.Factory converter, @Named(NAME_BASE_URL) String baseUrl) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(converter)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    UsdaApi provideUsdaApi(Retrofit retrofit){
+        return retrofit.create(UsdaApi.class);
     }
 
 }
